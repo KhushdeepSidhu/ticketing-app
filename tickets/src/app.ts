d48be976c95031baf280ac7bc0ee63554e93720e
sidhu <khushdeep>
2023-11-09 12:23:18 -0500

    added create ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-
 import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signupRouter } from './routes/signup';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
 import { errorHandler, NotFoundError } from '@khushdeeptickets/common';
+import { currentUser } from '@khushdeeptickets/common';
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -30,10 +28,9 @@ app.use(
   })
 );
 
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
